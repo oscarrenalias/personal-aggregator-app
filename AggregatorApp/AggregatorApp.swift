29f429c23b27da355380a6f896dceb8292a9d0da
Renalias, Oscar <oscar@renalias.net>
2026-06-22 12:40:14 +0300

    [takt] B-d10d6075: ContentSource configuration intent

--- AggregatorApp/AggregatorApp.swift
+++ AggregatorApp/AggregatorApp.swift
@@ -2,10 +2,19 @@ import SwiftUI
 
 @main
 struct AggregatorApp: App {
-    @State private var credentialsStore = CredentialsStore()
+    @State private var credentialsStore: CredentialsStore
     @State private var seenStore = ThreadSeenStore()
     @State private var listPreferences = ListPreferences()
     @State private var readStore = ArticleReadStore()
+
+    init() {
+        // Migration must run before CredentialsStore reads from the keychain.
+        KeychainHelper.migrateToSharedGroupIfNeeded(keys: [
+            "aggregator.clientId",
+            "aggregator.clientSecret"
+        ])
+        _credentialsStore = State(initialValue: CredentialsStore())
+    }
 
     var body: some Scene {
         WindowGroup {
